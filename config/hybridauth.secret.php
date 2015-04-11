ad2556bd8e36594c065a47664fa38d7bec527a1d
thinkingmedia <mathew@thinkingmedia.ca>
2015-04-11 01:36:12 -0400

    removed

--- config/hybridauth.secret.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// production
-//$facebook_id = '835187193222813';
-//$facebook_secret = '48477d5b4a71db1f433b15e36cf2332c';
-
-// test
-$facebook_id = '835198313221701';
-$facebook_secret = 'abf5dd15e34a1c243ee14fc57705b4ed';
